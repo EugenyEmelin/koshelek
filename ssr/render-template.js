1ae4fbd166278c7368b3ad827c854dbe041dc311
Evgeny Emelin <emelin.eug2@ya.ru>
2024-05-07 15:15:19 +0300

    Updates

---
+++ ssr/render-template.js
@@ -0,0 +1,19 @@
+export default function(ssrContext) {
+var __t, __p = '';
+__p += '<!DOCTYPE html><html ' +
+((__t = ( ssrContext._meta.htmlAttrs )) == null ? '' : __t) +
+'><head>' +
+((__t = ( ssrContext._meta.headTags )) == null ? '' : __t) +
+' <title>Koshelek</title><meta charset=utf-8><meta name=description content="Koshelek binance app"><meta name=format-detection content="telephone=no"><meta name=msapplication-tap-highlight content=no><meta name=viewport content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width"><link rel=icon type=image/png sizes=128x128 href=/icons/favicon-128x128.png><link rel=icon type=image/png sizes=96x96 href=/icons/favicon-96x96.png><link rel=icon type=image/png sizes=32x32 href=/icons/favicon-32x32.png><link rel=icon type=image/png sizes=16x16 href=/icons/favicon-16x16.png><link rel=icon type=image/ico href=/favicon.ico><script type=module crossorigin src=/assets/index-BnyblyMK.js></script><link rel=stylesheet crossorigin href=/assets/index-CeYCvZL8.css> ' +
+((__t = ( ssrContext._meta.endingHeadTags || '' )) == null ? '' : __t) +
+'</head><body class="' +
+((__t = ( ssrContext._meta.bodyClasses )) == null ? '' : __t) +
+'" ' +
+((__t = ( ssrContext._meta.bodyAttrs )) == null ? '' : __t) +
+'>' +
+((__t = ( ssrContext._meta.bodyTags )) == null ? '' : __t) +
+' <div id=q-app>' +
+((__t = ( ssrContext._meta.runtimePageContent )) == null ? '' : __t) +
+'</div></body></html>';
+return __p
+}
